f5129d327c22fc7d105ba337d27dc6b42ad4cfa2
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-23 11:09:11 +0100

    Created component App

--- pacherpacher/client/index.js
+++ pacherpacher/client/index.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '/imports/routers/App'
+
 console.log('CLIENT : Meteor est lancé')
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')    
+)
