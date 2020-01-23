dc4d02b236bc20b028843cba42bf40f0b812df8d
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-23 15:09:37 +0100

    Added Signup and semantic-ui

--- pacherpacher/imports/pages/Signup.js
+++ pacherpacher/imports/pages/Signup.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Form, Button, Container }  from 'semantic-ui-react'
+
 
 function Signup(props){
+    const [email, setEmail] = useState()
+    const [password, setPassword]   = useState()
     return(
-        <p>SIGNUP PAGE</p>
+        <Container>
+            <h1>Inscrivez-vous !</h1>
+            <Form>
+                <Form.Input type="email" onChange={(e, {value}) => setEmail(value)} label="Email" value={email} />
+                <Form.Input type="password" onChange={(e, {value}) => setPassword(value)} label="Mot de passe" value={password} />
+                <Button disabled={!email || !password}>M'inscrire</Button>
+            </Form>
+        </Container>
     )
 }
 
