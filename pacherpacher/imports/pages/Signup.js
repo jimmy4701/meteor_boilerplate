31c0ba0616053b01d46ba6a5ee44ec37f5673738
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-23 15:34:38 +0100

    Added Accounts password package and signup process

--- pacherpacher/imports/pages/Signup.js
+++ pacherpacher/imports/pages/Signup.js
@@ -1,14 +1,30 @@
 import React, {useState} from 'react'
 import { Form, Button, Container }  from 'semantic-ui-react'
+import { Accounts } from 'meteor/accounts-base'
 
 
 function Signup(props){
     const [email, setEmail] = useState()
     const [password, setPassword]   = useState()
+
+    const signup = () => {
+        console.log('SIGNUP', email, password)
+        Accounts.createUser({
+            email,
+            password
+        }, (err) => {
+            if(err){
+                alert(err.message)
+            }else{
+                console.log('Utilisateur créé !!!')
+            }
+        })
+    }
+
     return(
         <Container>
             <h1>Inscrivez-vous !</h1>
-            <Form>
+            <Form onSubmit={signup}>
                 <Form.Input type="email" onChange={(e, {value}) => setEmail(value)} label="Email" value={email} />
                 <Form.Input type="password" onChange={(e, {value}) => setPassword(value)} label="Mot de passe" value={password} />
                 <Button disabled={!email || !password}>M'inscrire</Button>
