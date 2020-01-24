613e98f3fda6dae5de991836ed9c1e88487d5ab0
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-24 14:32:39 +0100

    Created Product partial in components

---
+++ pacherpacher/imports/api/products/server/publications.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor'
+import Products from '/imports/api/products/products'
+
+
+Meteor.publish('products.me', function(){
+    return Products.find({seller: this.userId})
+})
+
--- pacherpacher/imports/pages/MyProducts.js
+++ pacherpacher/imports/pages/MyProducts.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Button, Container } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { withTracker } from 'meteor/react-meteor-data'
+import Products from '/imports/api/products/products'
+import Product from '/imports/components/Product'
 
 function MyProducts(props){
     return(
@@ -9,8 +12,20 @@ function MyProducts(props){
             <Link to="/new_product">
                 <Button color="green" icon="plus" content="Ajouter produit" />
             </Link>
+            {!props.loading && props.products.map(product => {
+                return <Product product={product} />
+            })}
         </Container>
     )
 }
 
-export default MyProducts
+export default withTracker(() => {
+    const products_pub = Meteor.subscribe('products.me')
+    const loading = !products_pub.ready()
+    const products = Products.find({seller: Meteor.userId()}).fetch()
+
+    return {
+        loading,
+        products
+    }
+})(MyProducts)
