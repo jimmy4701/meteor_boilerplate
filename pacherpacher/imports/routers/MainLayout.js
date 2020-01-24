0708e408d4d921ba687d95e60f2e972618f03563
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-24 12:09:40 +0100

    Added NewProduct page

--- pacherpacher/imports/routers/MainLayout.js
+++ pacherpacher/imports/routers/MainLayout.js
@@ -3,10 +3,16 @@ import {
     Switch,
     Route
 }                   from 'react-router-dom'
+
+// Standard routes
 import Landing      from '/imports/pages/Landing'
 import Signup       from '/imports/pages/Signup'
 import Signin       from '/imports/pages/Signin'
+import MyProducts   from '/imports/pages/MyProducts'
+import NewProduct   from '/imports/pages/NewProduct'
 import NotFound     from '/imports/pages/NotFound'
+
+// Admin routes
 import AdminUsers   from '/imports/pages/admin/AdminUsers'
 
 
@@ -16,6 +22,8 @@ function MainLayout(props){
             <Route path="/" exact component={Landing} />
             <Route path="/signup" component={Signup} />
             <Route path="/signin" component={Signin} />
+            <Route path="/my_products" component={MyProducts} />
+            <Route path="/new_product" component={NewProduct} />
             <Route path="/admin/users" component={AdminUsers} />
             <Route path="*" component={NotFound} />
         </Switch>
