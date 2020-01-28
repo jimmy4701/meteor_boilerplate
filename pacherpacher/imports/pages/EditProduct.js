5aadc07fb4884536bb77f6fadb8272794e46e85a
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-01-28 11:39:22 +0100

    Added Loader on loading of product

--- pacherpacher/imports/pages/EditProduct.js
+++ pacherpacher/imports/pages/EditProduct.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react'
-import { Form, Container, Button } from 'semantic-ui-react'
+import { Container, Loader  } from 'semantic-ui-react'
 import ProductForm from '/imports/components/ProductForm'
 
 function EditProduct(props){
-    const [product, setProduct] = useState({})
+    const [product, setProduct] = useState()
 
     useEffect(() => {
         console.log('props', props)
@@ -19,7 +19,11 @@ function EditProduct(props){
     return(
         <Container>
             <h1>Edition d'un produit</h1>
-            <ProductForm product={product} />
+            {!product ?
+                 <Loader active inline />
+            :
+                <ProductForm product={product} />
+            }
         </Container>
     )
 }
